9040852b433de6af8881f98cd88b2a5b2679efc5
DorottyaVarju <varjudorottya@gmail.com>
2024-12-15 19:37:01 +0100

    ESLint directive

--- src/components/LettersToTry.js
+++ src/components/LettersToTry.js
@@ -14,6 +14,7 @@ function LettersToTry(props) {
         }
     };
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
 
         document.addEventListener('keyup', handleKeyUp);
--- src/components/WordToGuess.js
+++ src/components/WordToGuess.js
@@ -155,6 +155,7 @@ function WordToGuess() {
         return [linesForWordToGuess, word];
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         returnAWordToGuess();
     }, []);
