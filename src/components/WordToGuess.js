867da2da093ddd464c6982df0919c739fc98595c
DorottyaVarju <varjudorottya@gmail.com>
2024-12-22 17:51:48 +0100

    back to mainpage

--- src/components/WordToGuess.js
+++ src/components/WordToGuess.js
@@ -37,7 +37,7 @@ function WordToGuess() {
     }
 
     const backToMainPage = () => {
-        window.location.href = '../';
+        window.location.href = '../12316582-1147284/index.html';
     };
     
 
